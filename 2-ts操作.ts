80d1a0956bbd4340fa657d6ad7a4d625fdc88cce
king <102906132+Ggy-king@users.noreply.github.com>
2022-08-30 21:32:19 +0800

    Add files via upload

---
+++ 2-ts操作.ts
@@ -0,0 +1,8 @@
+// npm i -g typescript 安装编译tsc文件
+//1 创建  hello.ts文件
+//2 将ts编译为js文件 输入命令 tsc hello.ts (此时同级目录下会出现一个同名js文件)
+//3 执行js代码 在终端中输入命令 node hello.js
+
+/**
+ * 1 所有合法的js代码都是ts代码
+ */
---
+++ 3-hello.ts
@@ -0,0 +1,3 @@
+console.log('hello world')
+let age: number = 18
+console.log(age)
