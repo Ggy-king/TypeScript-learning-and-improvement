3a2af88dd39b73fd514844060e70e32d5941dd8e
king <102906132+Ggy-king@users.noreply.github.com>
2022-10-17 21:24:14 +0800

    Add files via upload

---
+++ 37-在react中使用ts的概述.ts
@@ -0,0 +1,7 @@
+/**
+ * 以react为例
+ * 使用CRA创建ts项目(cra就是脚手架的简称)
+ * ts配置tsconfig.json
+ * react中的常见类型
+ */
+
---
+++ 38-使用CRA创建项目.ts
@@ -0,0 +1,10 @@
+// react脚手架工具
+/**
+ * 创建支持ts的项目命令 npx create-react-app 项目名称 --template typescript
+ * 或者在现有的react中添加ts包
+ */
+
+// 1 项目目录中增加了tsconfig.json配置文件:指定ts的编译选项
+// 2 react组件的文件扩展名变为.tsx
+// 3 src目录中增加了react-app-env.d.ts:react项目默认的类型声明文件{ ///三斜线指令 指定依赖的其他类型声明的文件 types表示依赖的类型声明文件包的名称 }
+
