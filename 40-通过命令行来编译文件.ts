b2cd60358d2a5f4034deb0e77f7fdbf4162fd392
king <102906132+Ggy-king@users.noreply.github.com>
2022-10-18 22:00:53 +0800

    Add files via upload

---
+++ 40-通过命令行来编译文件.ts
@@ -0,0 +1,3 @@
+// tsc hello.ts --target es6  执行文件
+// 1 tsc后带有输入文件时 会忽略tsconfig.json配置文件
+// 2 tsc后不带输入文件时 才会开启tsconfig.json
---
+++ 41-react中的常用类型.ts
@@ -0,0 +1,8 @@
+// 前提说明 现在 基于class组件来讲解react+ts 最新的react hooks 后面再说
+// 在不使用ts时 可以使用prop-types库 为react组件类型提供类型检查
+/**
+ * 1 ts项目中 推荐使用ts实现组件类型校验 (代替PropsTypes)
+ * 2 不管是vue还是react 只要是支持ts 都有很多库满足对类型的要求
+ * 3 react项目中通过@type/react、@types/react-dom类型声明包 提供类型
+ * 4 这些包CRA已经帮我们安装好了(react-app-env.d.ts)
+ */
